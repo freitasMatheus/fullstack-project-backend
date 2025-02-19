538470efb99de9408abb8b5bf18dfde84b50e805
Matheus de Freitas <matt.freitas01@gmail.com>
2025-02-19 20:54:55 -0300

    Add Helmet and enable CORS support with dynamic configuration
    Integrated Helmet for enhanced security headers and configured CORS with support for dynamic origins based on environment variables. Updated Docker setup for local development and improved environment file handling for flexible configurations.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { config } from 'dotenv';
-config();
+import { join } from 'path';
+
+const envFile = `.env.${process.env.NODE_ENV || 'local'}`;
+config({ path: join(__dirname, '..', envFile) });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const allowedOrigins = process.env.CORS_ORIGIN?.split(',').map((origin) => origin.trim()) || [];
+  app.use(helmet());
+
+  app.enableCors({
+    origin: allowedOrigins.length ? allowedOrigins : false,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
 }
+
 bootstrap();
